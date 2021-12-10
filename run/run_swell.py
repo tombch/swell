4bc23fb44c77d3ac19a33ebda9571cd15b1787d3
tombch <tombrier@hotmail.co.uk>
2021-12-10 14:22:40 +0000

    added graph-path argument and changed print statement presentation in run_swell

--- run/run_swell.py
+++ run/run_swell.py
@@ -9,17 +9,18 @@ import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-l", "--latest-dir")
-parser.add_argument("-o", "--out-dir", help="Directory to save fasta/metadata files", type=pathlib.Path, metavar="")
+parser.add_argument("-o", "--out-dir", help="Directory to save fasta/metadata files", type=pathlib.Path)
+parser.add_argument("-g", "--graph-path")
 parser.add_argument("--swell-bam", help="", action="store_true")
 parser.add_argument("-b", "--bed")
 parser.add_argument("-s", "--start-date")
 parser.add_argument("-e", "--end-date")
 args = parser.parse_args()
 
-print("Reading metadata into DataFrame...")
+print("Reading metadata into DataFrame... ", end="")
 metadata_path = f"{args.latest_dir}majora.metadata.matched.tsv"
 metadata = pd.read_csv(metadata_path, sep="\t", low_memory=False)
-print("Done.")
+print("done.")
 
 prefix = f"swell_data_{args.start_date}_{args.end_date}"
 out_fasta_path = f"{args.out_dir}/{prefix}.fasta"
@@ -28,7 +29,7 @@ out_metadata_path = f"{args.out_dir}/{prefix}_metadata.tsv"
 
 metadata["sequencing_submission_date"] = pd.to_datetime(metadata["sequencing_submission_date"], errors="coerce")
 if args.start_date or args.end_date:
-    print("Filtering metadata by given start/end dates...")
+    print("Filtering metadata by given start/end dates... ", end="")
     if args.start_date and args.end_date:
         mask = (metadata['sequencing_submission_date'] >= args.start_date) & (metadata['sequencing_submission_date'] <= args.end_date)
     elif args.start_date:
@@ -36,12 +37,12 @@ if args.start_date or args.end_date:
     elif args.end_date:
         mask = (metadata['sequencing_submission_date'] <= args.end_date)
     metadata = metadata.loc[mask]
-    print("Done.")
-print("Saving metadata to tsv in out-dir...")
+    print("done.")
+print("Saving metadata to tsv in out-dir... ", end="")
 metadata.to_csv(out_metadata_path, index=False, sep='\t')
-print("Done.")
+print("done.")
 
-print("Writing fasta data to multifasta (and optional swell of bam data to a tsv)...")
+print("Writing fasta data to multifasta (and optional swell of bam data to a tsv)... ", end="")
 with open(out_fasta_path, "w") as out_fasta, open(out_bam_data_path, "w") as out_bam:
     for i, (index, row) in enumerate(metadata.iterrows()):
         fasta_path = f"{args.latest_dir}fasta/{row['central_sample_id']}.{row['run_name']}.climb.fasta"
@@ -62,11 +63,11 @@ with open(out_fasta_path, "w") as out_fasta, open(out_bam_data_path, "w") as out
                 out_bam.write(f"{data}\n")
             else:
                 out_bam.write(f"{data}\n")
-print("Done.")
-print("Running swell on multifasta...")
+print("done.")
+print("Running swell on multifasta... ", end="")
 swell_fasta_out = subprocess.run(['swell', 'separate-fasta', out_fasta_path], capture_output=True)
-print("Done.")
-print("Generating fasta graphs...")
-subprocess.run(['Rscript', '../separate_fasta_plot.R', out_metadata_path], input=swell_fasta_out.stdout)
-print("Done.")
+print("done.")
+print("Generating fasta graphs... ", end="")
+subprocess.run(['Rscript', args.graph_path, out_metadata_path], input=swell_fasta_out.stdout)
+print("done.")
 subprocess.run(['rm', out_fasta_path, out_metadata_path])
