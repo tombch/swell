499d2a3ac3eb8b00de34562db8d5a8a6392e71e8
tombch <tombrier@hotmail.co.uk>
2021-12-14 16:43:51 +0000

    File renaming, removal of COG-UK string substitution in swell, and updated multi_swell file

--- separate_fasta_plot.R
+++ separate_fasta_plot.R
@@ -8,6 +8,7 @@ args <- commandArgs(trailingOnly = TRUE)
 swell <- read.table(file("stdin"), head=TRUE, sep='\t', na.strings=c("NaN", "-"))
 metadata <- read.table(args[1], head=TRUE, sep='\t', fill=TRUE)
 df <- merge(x=swell, y=metadata, by.x="header", by.y="fasta_header")
+
 # Calculate ISO week for each resulting entry
 df$sequencing_submission_week = as.integer(strftime(df$sequencing_submission_date, format="%V"))
 
