ad17d4756880a981b4a2636398ff1dfa8fcb86c3
tombch <tombrier@hotmail.co.uk>
2021-12-14 17:56:36 +0000

    separate_fasta_plot now has for loop to separate graphs of runs by sequencing org

--- separate_fasta_plot.R
+++ separate_fasta_plot.R
@@ -13,73 +13,76 @@ df <- merge(x=swell, y=metadata, by.x="header", by.y="fasta_header")
 df$sequencing_submission_week = as.integer(strftime(df$sequencing_submission_date, format="%V"))
 
 # Percentage acgt scatterplot, sequencing org on x axis
-ggplot(data = df, mapping = aes(x = sequencing_org_code, y = pc_acgt)) +
+print(ggplot(data = df, mapping = aes(x = sequencing_org_code, y = pc_acgt)) +
     geom_point(alpha = alpha_val) + 
-    theme_bw()
-# Percentage ambiguous scatterplot, sequencing org on x axis
-ggplot(data = df, mapping = aes(x = sequencing_org_code, y = pc_ambiguous)) +
-    geom_point(alpha = alpha_val) + 
-    theme_bw()
-
+    theme_bw())
 # Percentage acgt scatterplot, faceted by sequencing org
-ggplot(data = df, mapping = aes(x = pc_acgt, y = 0)) +
+print(ggplot(data = df, mapping = aes(x = pc_acgt, y = 0)) +
     geom_jitter(width = 0, alpha = alpha_val) +
     facet_wrap(. ~ sequencing_org_code, ncol=3, scale="free_y") + 
     scale_y_continuous(breaks = NULL) +
     theme_bw() +
-    theme(axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank(), panel.grid.major.y=element_blank()) 
-# Percentage ambiguous scatterplot, faceted by sequencing org
-ggplot(data = df, mapping = aes(x = pc_ambiguous, y = 0)) +
-    geom_jitter(width = 0, alpha = alpha_val) +
-    facet_wrap(. ~ sequencing_org_code, ncol=3, scale="free_y") + 
-    scale_y_continuous(breaks = NULL) +
-    theme_bw() +
-    theme(axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank(), panel.grid.major.y=element_blank())
-
-# Percentage acgt scatterplot, faceted by sequencing org and run name
-ggplot(data = df, mapping = aes(x = pc_acgt, y = 0)) +
-    geom_jitter(width = 0, alpha = alpha_val) +
-    facet_wrap(sequencing_org_code ~ run_name, ncol=2, scale="free_y") + 
-    scale_y_continuous(breaks = NULL) +
-    theme_bw() +
-    theme(axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank(), panel.grid.major.y=element_blank()) 
-# Percentage ambiguous scatterplot, faceted by sequencing org and run name
-ggplot(data = df, mapping = aes(x = pc_ambiguous, y = 0)) +
-    geom_jitter(width = 0, alpha = alpha_val) +
-    facet_wrap(sequencing_org_code ~ run_name, ncol=2, scale="free_y") + 
-    scale_y_continuous(breaks = NULL) +
-    theme_bw() +
-    theme(axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank(), panel.grid.major.y=element_blank())
-
+    theme(axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank(), panel.grid.major.y=element_blank()))
 # Percentage acgt histogram, faceted by sequencing org
-ggplot(data = df, mapping = aes(x = pc_acgt)) +
+print(ggplot(data = df, mapping = aes(x = pc_acgt)) +
     geom_histogram() + 
     facet_wrap(. ~ sequencing_org_code, ncol=3, scale="free_y") + 
-    theme_bw()
-# Percentage ambiguous histogram, faceted by sequencing org
-ggplot(data = df, mapping = aes(x = pc_ambiguous)) +
-    geom_histogram() +
-    facet_wrap(. ~ sequencing_org_code, ncol=3, scale="free_y") + 
-    theme_bw()
-
-# Percentage acgt histogram, faceted by sequencing org and run name
-ggplot(data = df, mapping = aes(x = pc_acgt)) +
-    geom_histogram() + 
-    facet_wrap(sequencing_org_code ~ run_name, ncol=2, scale="free_y") + 
-    theme_bw()
-# Percentage ambiguous histogram, faceted by sequencing org and run name
-ggplot(data = df, mapping = aes(x = pc_ambiguous)) +
-    geom_histogram() +
-    facet_wrap(sequencing_org_code ~ run_name, ncol=2, scale="free_y") + 
-    theme_bw()
-
+    theme_bw())
 # Average pc_acgt per ISO week, coloured by sequencing org
-ggplot(data = df, mapping = aes(x = sequencing_submission_week, y = pc_acgt, colour = sequencing_org_code)) + 
+print(ggplot(data = df, mapping = aes(x = sequencing_submission_week, y = pc_acgt, colour = sequencing_org_code)) + 
     geom_point(stat = "summary", fun = "mean") + 
     geom_line(stat = "summary", fun = "mean") + 
-    labs(y = "avg")
+    labs(y = "avg"))
+
+# Percentage ambiguous scatterplot, sequencing org on x axis
+print(ggplot(data = df, mapping = aes(x = sequencing_org_code, y = pc_ambiguous)) +
+    geom_point(alpha = alpha_val) + 
+    theme_bw())
+# Percentage ambiguous scatterplot, faceted by sequencing org
+print(ggplot(data = df, mapping = aes(x = pc_ambiguous, y = 0)) +
+    geom_jitter(width = 0, alpha = alpha_val) +
+    facet_wrap(. ~ sequencing_org_code, ncol=3, scale="free_y") + 
+    scale_y_continuous(breaks = NULL) +
+    theme_bw() +
+    theme(axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank(), panel.grid.major.y=element_blank()))
+# Percentage ambiguous histogram, faceted by sequencing org
+print(ggplot(data = df, mapping = aes(x = pc_ambiguous)) +
+    geom_histogram() +
+    facet_wrap(. ~ sequencing_org_code, ncol=3, scale="free_y") + 
+    theme_bw())
 # Average pc_ambiguous per ISO week, coloured by sequencing org
-ggplot(data = df, mapping = aes(x = sequencing_submission_week, y = pc_ambiguous, colour = sequencing_org_code)) + 
+print(ggplot(data = df, mapping = aes(x = sequencing_submission_week, y = pc_ambiguous, colour = sequencing_org_code)) + 
     geom_point(stat = "summary", fun = "mean") + 
     geom_line(stat = "summary", fun = "mean") + 
-    labs(y = "avg")
+    labs(y = "avg"))
+
+# Get unique sequencing_org_codes
+seq_org_codes <- unique(df$sequencing_org_code)
+# Loop through sequencing_org_codes to create separate graphs for each
+for(code in seq_org_codes){
+    current_df <- filter(df, sequencing_org_code == code)
+    # Percentage acgt scatterplot, faceted by sequencing org and run name
+    print(ggplot(data = current_df, mapping = aes(x = pc_acgt, y = 0)) +
+        geom_jitter(width = 0, alpha = alpha_val) +
+        facet_wrap(sequencing_org_code ~ run_name, ncol=3, scale="free_y") + 
+        scale_y_continuous(breaks = NULL) +
+        theme_bw() +
+        theme(axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank(), panel.grid.major.y=element_blank()))
+    # Percentage acgt histogram, faceted by sequencing org and run name
+    print(ggplot(data = current_df, mapping = aes(x = pc_acgt)) +
+        geom_histogram() + 
+        facet_wrap(sequencing_org_code ~ run_name, ncol=3, scale="free_y") + 
+        theme_bw())
+    # Percentage ambiguous scatterplot, faceted by sequencing org and run name
+    print(ggplot(data = current_df, mapping = aes(x = pc_ambiguous, y = 0)) +
+        geom_jitter(width = 0, alpha = alpha_val) +
+        facet_wrap(sequencing_org_code ~ run_name, ncol=3, scale="free_y") + 
+        scale_y_continuous(breaks = NULL) +
+        theme_bw() +
+        theme(axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank(), panel.grid.major.y=element_blank()))
+    # Percentage ambiguous histogram, faceted by sequencing org and run name
+    print(ggplot(data = current_df, mapping = aes(x = pc_ambiguous)) +
+        geom_histogram() +
+        facet_wrap(sequencing_org_code ~ run_name, ncol=3, scale="free_y") + 
+        theme_bw())
+}
